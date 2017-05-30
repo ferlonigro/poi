e4f544a954ea7ded4780e1adbd0e3e511938d285
Pablo Chamorro <pablo.chamorro@worldline.com>
2017-05-30 17:13:48 -0300

    commit pablo

--- src/domain/POI.java
+++ src/domain/POI.java
@@ -1,20 +1,29 @@
 package domain;
 
 public class POI {
+	private String servicio;
 	private long id;
 	private String nombre;
 	private String descripcion;
 	private double latitud;
 	private double longitud;
 	
-	public POI(long id, String nombre, String descripcion, long latitud, long longitud) {
+	public POI(String servicio, long id, String nombre, String descripcion, long latitud, long longitud) {
+		this.servicio = servicio;
 		this.id = id;
 		this.nombre = nombre;
 		this.descripcion = descripcion;
 		this.latitud = latitud;
 		this.longitud = longitud;
 	}
-	
+
+
+	public String getServicio() {
+		return servicio;
+	}
+
+
+
 	public long getId() {
 		return id;
 	}
